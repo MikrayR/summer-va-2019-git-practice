a5f319ef1f4f1f0ed32e7aa344b4b7cff35e86a8
Mikray Rahman <milareheman@gmail.com>
2019-11-21 16:44:47 -0500

    initial commit

---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+
+    public static void main(String[] args) {
+        System.out.println("I am learning Git-Hub ");
+        System.out.println("I am still need to learn about this");
+    }
+}
