45d9f3bf7229365469a519777a7768dd623611cc
Mikray Rahman <milareheman@gmail.com>
2019-12-05 16:31:04 -0500

    new line added

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -4,5 +4,6 @@ public class SecondClass {
         System.out.println("I am learning Git-Hub ");
         System.out.println("I am still need to learn about this");
         System.out.println("Sherinay did great job! for preparation for Luis Bday.gi");
+        System.out.println("in this class we learn how to fix confilict");
     }
 }
