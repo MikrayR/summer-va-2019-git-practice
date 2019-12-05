15ab15a769515fd9fd4f628b1f065ea4efa3277b
Mikray Rahman <milareheman@gmail.com>
2019-12-05 15:48:41 -0500

    change a line

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -3,6 +3,6 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("I am learning Git-Hub ");
         System.out.println("I am still need to learn about this");
-        System.out.println("Hi I did I git pull");
+        System.out.println("Sherinay did great job! for preparation for Luis Bday.gi");
     }
 }
