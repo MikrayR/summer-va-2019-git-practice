f1f7fa6d482a7dba1d328010af70c524c68e636f
Mikray Rahman <milareheman@gmail.com>
2019-12-18 12:08:32 -0500

    update

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -6,8 +6,7 @@ public class SecondClass {
         System.out.println("Sherinay did great job! for preparation for Luis Bday.gi");
         System.out.println("in this class we learn how to fix confilict");
         System.out.println("in this class we learn how to fix confilict");
-        System.out.println("in this class we learn how to fix confilict");
-        System.out.println("in this class we learn how to fix confilict");
+        System.out.println("confilict");
 
 
     }
